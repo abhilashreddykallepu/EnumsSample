c266cf046ddd46cb2cad80ed64e767b8f9bc1bdf
abhiii <HadiHatinoglu@Hadis-iMac.local>
2013-11-12 11:15:56 +0530

    Initial Commit

---
+++ EnumsTests/EnumsTests.m
@@ -0,0 +1,34 @@
+//
+//  EnumsTests.m
+//  EnumsTests
+//
+//  Created by Hadi Hatinoglu on 12/11/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EnumsTests : XCTestCase
+
+@end
+
+@implementation EnumsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
